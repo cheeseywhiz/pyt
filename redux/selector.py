a62368977e85fbcdcd0d160e61bd18f5c96835ba
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-08-03 14:02:20 -0400

    Add selector cache

--- redux/selector.py
+++ redux/selector.py
@@ -3,12 +3,23 @@ __all__ = 'selector',
 
 def selector(*input_selectors):
     def decorator(func):
+        cache_state = None
+        cache_result = None
+
         def wrapper(state):
+            nonlocal cache_state, cache_result
+
+            if state is cache_state:
+                return cache_result
+            else:
+                cache_state = state
+
             args = (
                 input_selector(state)
                 for input_selector in input_selectors
             )
-            return func(*args)
+            cache_result = func(*args)
+            return cache_result
 
         return wrapper
 
