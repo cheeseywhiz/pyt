9bdbd51286a0e64d6f818c54ddd9ee5b538e2de2
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-08-03 23:52:16 -0400

    Reorganize Terminal reducer

--- pyt/Terminal.py
+++ pyt/Terminal.py
@@ -7,42 +7,40 @@ from . import control_codes
 __all__ = 'Terminal',
 
 
-class Lines(redux.Reducer[typing.List[str]]):
-    field = dataclasses.field(
+@dataclasses.dataclass(frozen=True)
+class Terminal(redux.CombineReducers):
+    lines: typing.List[str] = dataclasses.field(
         default_factory=lambda: [''],
     )
 
-    def add_char(state, char):
-        state[-1] += char
-        return state
+    def copy(self):
+        return type(self)(self.lines[:])
 
-    def add_C0(state, C0):
+    def add_char(self, char):
+        self.lines[-1] += char
+        return self
+
+    def add_C0(self, C0):
         if C0 in (
                 control_codes.C0.LF,
                 control_codes.C0.VT,
                 control_codes.C0.FF,
         ):
-            state.append('')
+            self.lines.append('')
 
-        return state
+        return self
 
-    def put_char(state, char):
+    def put_char(self, char):
         code_point = ord(char)
 
         if code_point in control_codes.C0:
             C0 = control_codes.C0(code_point)
-            return Lines.add_C0(state, C0)
+            return self.add_C0(C0)
 
-        return Lines.add_char(state, char)
+        return self.add_char(char)
 
-    def reduce(state=None, action=None):
+    def reduce(self, action=None):
         if isinstance(action, actions.PutChar):
-            return Lines.put_char(state[:], action.char)
+            return self.copy().put_char(action.char)
 
-        return state
-
-
-@dataclasses.dataclass(frozen=True)
-@redux.init_reducers
-class Terminal(redux.CombineReducers):
-    lines: Lines
+        return self
