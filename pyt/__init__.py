cbef6fbb156ea40e008e17d5b6a8337c19386787
Spencer Todd <cheesy.mac.dott@gmail.com>
2018-08-06 01:40:18 -0400

    Add unicode encoder/decoder

--- pyt/__init__.py
+++ pyt/__init__.py
@@ -2,5 +2,6 @@ from . import actions
 from . import control_codes
 from .Terminal import Terminal
 from .ParsedCSI import ParsedCSI
+from . import unicode
 
-__all__ = 'actions', 'control_codes', 'Terminal', 'ParsedCSI'
+__all__ = 'actions', 'control_codes', 'Terminal', 'ParsedCSI', 'unicode'
